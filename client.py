52fdea1db9c1aa5707c8fb0d5ae06033fbe776f6
Hamza Memon <hamzamemon72037@gmail.com>
2025-08-06 20:53:04 +0530

    client.py

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ model = genai.GenerativeModel("gemini-2.5-flash")
 
 def gemini_process_command(command):
     prompt = f"""
-You are Jarvis, a virtual Indian AI assistant created by Spartan. Use a friendly, sharp tone to respond smartly.
+You are Jarvis, a virtual Indian AI assistant created by Hamza. Use a friendly, sharp tone to respond smartly.
 Stay short, clear, and on-topic.
 
 User said:
@@ -24,3 +24,4 @@ Respond like a helpful assistant.
     except Exception as e:
         print("Gemini error:", e)
         return "Sorry, Gemini couldn't process your request."
+
